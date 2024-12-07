ba0b8aefea2f78ef102aa3e9aef1e1d3dd618d7e
edith.githinji <edithgithinji2020@gmail.com>
2024-12-07 17:53:35 +0300

    using filter() method

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, driverName) {
+    return drivers.filter(function(name) {
+      return name.toLowerCase()=== driverName.toLowerCase(); 
+    });
+  }
+  
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.startsWith(query));
+}
+
+function matchName(driver, name) {
+    return driver.filter(driver =>driver.name === name)
+}
